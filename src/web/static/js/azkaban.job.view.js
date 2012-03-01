de6f4f40c91d9481dd954307079947d27cbf5bd7
Richard Park <rpark@linkedin.com>
2012-03-01 13:14:20 -0800

    Added modal for upload job.

--- src/web/static/js/azkaban.job.view.js
+++ src/web/static/js/azkaban.job.view.js
@@ -2,32 +2,36 @@ $.namespace('azkaban');
 
 var jobView;
 azkaban.JobView= Backbone.View.extend({
-	events : {
-		"click #upload-btn":"handleUploadJob"
-	},
-	initialize : function(settings) {
+  events : {
+    "click #upload-btn":"handleUploadJob"
+  },
+  initialize : function(settings) {
 
-	},
-	handleUploadJob : function(evt) {
-		console.log("click upload");
-	},
-	render: function() {
-	}
-});
+  },
+  handleUploadJob : function(evt) {
+    $('#upload-job').modal({
+      closeHTML: "<a href='#' title='Close' class='modal-close'>x</a>",
+      position: ["20%",],
+      containerId: 'confirm-container',
+      containerCss: {
+        'height': '220px',
+        'width': '565px'
+      },
+      onShow: function (dialog) {
+        var modal = this;
 
-azkaban.UploadJobView= Backbone.View.extend({
-	events : {
-	},
-	initialize : function(settings) {
-
-	},
-	handleUploadJob : function(evt) {
-		
-	},
-	render: function() {
-	}
+        // if the user clicks "yes"
+        $('.yes', dialog.data[0]).click(function () {
+          // close the dialog
+          modal.close(); // or $.modal.close();
+        });
+      }
+    });
+  },
+  render: function() {
+  }
 });
 
 $(function() {
-	jobView = new azkaban.JobView({el:$( '#all-jobs-content' )});
+  jobView = new azkaban.JobView({el:$( '#all-jobs-content' )});
 });
