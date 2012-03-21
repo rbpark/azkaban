68e4316798ab8a9f62daf753e2b15b9c88adc460
Richard Park <rpark@linkedin.com>
2012-03-21 12:06:49 -0700

    Adding Hdfs browser

--- azkaban/src/java/azkaban/web/pages/HdfsBrowserServlet.java
+++ azkaban/src/java/azkaban/web/pages/HdfsBrowserServlet.java
@@ -50,8 +50,8 @@ public class HdfsBrowserServlet extends AbstractAzkabanServlet {
 
     private static final long serialVersionUID = 1;
 
-    private FileSystem _fs;
-
+    //private FileSystem _fs;
+    private Configuration conf;
     private ArrayList<HdfsFileViewer> _viewers = new ArrayList<HdfsFileViewer>();
 
     // Default viewer will be a text viewer
@@ -68,13 +68,9 @@ public class HdfsBrowserServlet extends AbstractAzkabanServlet {
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
-        try {
-            Configuration conf = new Configuration();
-            conf.setClassLoader(this.getApplication().getClassLoader());
-            _fs = FileSystem.get(conf);
-        } catch(IOException e) {
-            throw new ServletException(e);
-        }
+
+        conf = new Configuration();
+        conf.setClassLoader(this.getApplication().getClassLoader());
     }
 
     @Override
@@ -89,33 +85,37 @@ public class HdfsBrowserServlet extends AbstractAzkabanServlet {
         if(logger.isDebugEnabled())
             logger.debug("path=" + fsPath);
 
-        Path path = new Path(fsPath);
-        if(!_fs.exists(path))
-            throw new IllegalArgumentException(path.toUri().getPath() + " does not exist.");
-        else if(_fs.isFile(path))
-            displayFile(req, resp, path);
-        else if(_fs.getFileStatus(path).isDir())
-            displayDir(req, resp, path);
-        else
-            throw new IllegalStateException("It exists, it is not a file, and it is not a directory, what is it precious?");
-
+        FileSystem fs = FileSystem.get(conf);
+        
+        try {
+            Path path = new Path(fsPath);
+            if(!fs.exists(path)) {
+                throw new IllegalArgumentException(path.toUri().getPath() + " does not exist.");
+            }
+            else if(fs.isFile(path)) {
+                displayFile(fs, req, resp, path);
+            }
+            else if(fs.getFileStatus(path).isDir()) {
+                displayDir(fs, req, resp, path);
+            }
+            else {
+                throw new IllegalStateException("It exists, it is not a file, and it is not a directory, what is it precious?");
+            }
+        }
+        catch (Exception e) {
+            throw new IOException(e);
+        } finally {
+            logger.info("Close");
+            fs.close();
+        }
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-
-        String action = req.getParameter("action");
-        if("delete".equals(action)) {
-            Path theFile = new Path(req.getParameter("file"));
-            _fs.delete(theFile, true);
-        } else {
-            throw new ServletException("Unknown action '" + action + "'!");
-        }
-
     }
 
-    private void displayDir(HttpServletRequest req, HttpServletResponse resp, Path path)
+    private void displayDir(FileSystem fs, HttpServletRequest req, HttpServletResponse resp, Path path)
             throws IOException {
 
         Page page = newPage(req, resp, "azkaban/web/pages/hdfs_browser_dir.vm");
@@ -134,12 +134,12 @@ public class HdfsBrowserServlet extends AbstractAzkabanServlet {
 
         page.add("paths", paths);
         page.add("segments", segments);
-        page.add("subdirs", _fs.listStatus(path)); // ??? line
+        page.add("subdirs", fs.listStatus(path)); // ??? line
         page.render();
 
     }
 
-    private void displayFile(HttpServletRequest req, HttpServletResponse resp, Path path)
+    private void displayFile(FileSystem fs, HttpServletRequest req, HttpServletResponse resp, Path path)
             throws IOException {
 
         int startLine = WebUtils.getInt(req, "start_line", 1);
@@ -147,21 +147,32 @@ public class HdfsBrowserServlet extends AbstractAzkabanServlet {
 
         // use registered viewers to show the file content
         boolean outputed = false;
-        OutputStream output = resp.getOutputStream();
-        for(HdfsFileViewer viewer: _viewers) {
-            if(viewer.canReadFile(_fs, path)) {
-                viewer.displayFile(_fs, path, output, startLine, endLine);
-                outputed = true;
-                break; // don't need to try other viewers
+        OutputStream output = null;
+        try {
+            output = resp.getOutputStream();
+            for(HdfsFileViewer viewer: _viewers) {
+                if(viewer.canReadFile(fs, path)) {
+                    viewer.displayFile(fs, path, output, startLine, endLine);
+                    outputed = true;
+                    break; // don't need to try other viewers
+                }
+            }
+    
+            // use default text viewer
+            if(!outputed) {
+                if(_defaultViewer.canReadFile(fs, path)) {
+                    _defaultViewer.displayFile(fs, path, output, startLine, endLine);
+                } else {
+                    output.write(("Sorry, no viewer available for this file. ").getBytes("UTF-8"));
+                }
             }
         }
-
-        // use default text viewer
-        if(!outputed) {
-            if(_defaultViewer.canReadFile(_fs, path)) {
-                _defaultViewer.displayFile(_fs, path, output, startLine, endLine);
-            } else {
-                output.write(("Sorry, no viewer available for this file. ").getBytes("UTF-8"));
+        catch (IOException e) {
+            throw e;
+        }
+        finally {
+            if (output != null) {
+                output.close();
             }
         }
     }
